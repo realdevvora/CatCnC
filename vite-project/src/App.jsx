0e46dd6228f8555530bfa99f989a062c270d01b5
realdevvora <legitvoradev@gmail.com>
2023-12-20 17:45:43 -0500

    Created Project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="" className='flex items-center logo'>
+          <img src="/cat_logo.png" alt="logo" />
+          <span className='font-bold'>CatCnC</span>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
